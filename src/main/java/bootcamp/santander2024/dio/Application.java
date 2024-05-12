7b9b24c784575df79446b9306350454e2ea8e42b
jeferson freire <jfreire86@hotmail.com>
2024-05-12 11:13:18 -0300

    first commit

---
+++ src/main/java/bootcamp/santander2024/dio/Application.java
@@ -0,0 +1,13 @@
+package bootcamp.santander2024.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/santander2024/dio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.santander2024.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
